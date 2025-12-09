b0aaae6ec86a7af24ddcc61cbd7a4424d7d432cc
KrisD23 <krishandwive9@gmail.com>
2025-12-10 00:40:28 +0530

    add the provider to the root layout

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { QueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { useState } from "react";
 
-export const Providers = () => {
+export const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 };
