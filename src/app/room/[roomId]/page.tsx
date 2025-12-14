8053cd44bcf6c4d1bca9fba9e32b92d8bd9b5f6b
KrisD23 <krishandwive9@gmail.com>
2025-12-15 00:08:31 +0530

    add a realtime logic

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useUsername } from "@/hooks/use-username";
+import { client } from "@/lib/client";
+import { useMutation } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { useRef, useState } from "react";
 
@@ -12,10 +15,23 @@ function formatTimeRemaining(seconds: number) {
 const Page = () => {
   const params = useParams();
   const roomId = params.roomId as string;
+
+  const { username } = useUsername();
   const [copyStatus, setCopyStatus] = useState("COPY");
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
   const [input, setInput] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const { mutate: sendMessage, isPending } = useMutation({
+    mutationFn: async ({ text }: { text: string }) => {
+      await client.messages.post(
+        { sender: username, text },
+        { query: { roomId } }
+      );
+
+      setInput("");
+    },
+  });
 
   const copyLink = () => {
     const url = window.location.href;
@@ -88,7 +104,7 @@ const Page = () => {
               value={input}
               onKeyDown={(e) => {
                 if (e.key === "Enter" && input.trim()) {
-                  // Send message
+                  sendMessage({ text: input });
                   inputRef.current?.focus();
                 }
               }}
@@ -99,7 +115,11 @@ const Page = () => {
           </div>
 
           <button
-            onClick={() => {}}
+            onClick={() => {
+              sendMessage({ text: input });
+              inputRef.current?.focus();
+            }}
+            disabled={!input.trim() || isPending}
             className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
           >
             SEND
